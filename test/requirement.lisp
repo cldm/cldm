7bbd99894125f6a0f78e67a49ee008ae9929a34a
mmontone <marianomontone@gmail.com>
2014-05-10 14:03:35 -0300

    More requirements tests

--- test/requirement.lisp
+++ test/requirement.lisp
@@ -58,10 +58,53 @@
   (flet ((req (string)
 	   (read-requirement-from-string string)))
     (let ((requirement (req "hunchentoot >= 1.3.0, hunchentoot <= 1.4.0")))
+      ;; equality constraints
       (is (requirement-matches requirement (req "hunchentoot")))
       (is (not (requirement-matches requirement (req "hunchentoot == 1.2.0"))))
       (is (requirement-matches requirement (req "hunchentoot == 1.3.0")))
       (is (requirement-matches requirement (req "hunchentoot == 1.4.0")))
       (is (not (requirement-matches requirement (req "hunchentoot == 1.5.0"))))
 
-      
+      ;; different library name
+      (is (not (requirement-matches requirement (req "cl-ppcre == 130"))))
+
+      ;; range
+      (is (requirement-matches requirement (req "hunchentoot >= 1.3.0")))
+      (is (requirement-matches requirement (req "hunchentoot >= 1.4.0")))
+      (is (not (requirement-matches requirement (req "hunchentoot >= 1.5.0"))))
+
+      (is (requirement-matches requirement (req "hunchentoot >= 1.3.0, hunchentoot <= 1.5.0")))
+      (is (requirement-matches requirement (req "hunchentoot >= 1.4.0, hunchentoot <= 1.5.0")))
+      (is (not (requirement-matches requirement (req "hunchentoot >= 1.5.0, hunchentoot <= 1.6.0"))))
+      (is (requirement-matches requirement (req "hunchentoot >= 1.2.0, hunchentoot <= 1.3.0")))
+      (is (not (requirement-matches requirement (req "hunchentoot >= 1.2.0, hunchentoot <= 1.2.5")))))
+
+    (let ((requirement (req "hunchentoot == 1.3.0")))
+      (is (requirement-matches requirement (req "hunchentoot == 1.3.0")))
+      (is (not (requirement-matches requirement (req "hunchentoot == 1.2.0"))))
+      (is (requirement-matches requirement (req "hunchentoot >= 1.3.0")))
+      (is (requirement-matches requirement (req "hunchentoot <= 1.4.0"))))
+
+    (let ((requirement (req "hunchentoot > 1.3.0, hunchentoot < 1.4.0")))
+      (is (requirement-matches requirement (req "hunchentoot == 1.3.5")))
+      (is (not (requirement-matches requirement (req "hunchentoot == 1.3.0"))))
+      (is (not (requirement-matches requirement (req "hunchentoot == 1.4.0"))))
+      (is (requirement-matches requirement (req "hunchentoot >= 1.3.2, hunchentoot <= 1.3.4")))
+      (is (requirement-matches requirement (req "hunchentoot != 1.3.2"))))
+
+    (let ((req1 (req "hunchentoot >= 1.3.0, hunchentoot <= 1.5.0, hunchentoot <= 1.4.0"))
+	  (req2 (req "hunchentoot <= 1.3.0, hunchentoot <= 1.4.0")))
+      (is (requirement= req1 req2)))
+
+    (let ((requirement (req "hunchentoot >= 1.3.0, hunchentoot <= 1.2.0")))
+      (is (not (requirement-matches requirement (req "hunchentoot")))))
+
+    (let ((requirement (req "hunchentoot > 1.3.0, hunchentoot < 1.4.0")))
+      (is (requirement-matches requirement (req "hunchentoot")))
+      (is (requirement-matches requirement (req "hunchentoot == 1.3.5")))
+      (is (not (requirement-matches requirement (req "hunchentoot == 1.3.0"))))
+      (is (not (requirement-matches requirement (req "hunchentoot == 1.4.0")))))
+
+    (let ((requirement (req "hunchentoot == 1.3.0"))
+	  (lib-requirement (read-requirement-from-library-string "hunchentoot-1.3.0")))
+      (is (requirement= requirement lib-requirement)))))
